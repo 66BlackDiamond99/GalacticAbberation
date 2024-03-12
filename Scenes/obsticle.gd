03264cab6b9d19d046b9757dd2371b7d49bc9478
Black Diamond <6.black.diamond.9@gmail.com>
2024-03-12 18:35:45 +0200

    improvements on obstacles

--- Scenes/obsticle.gd
+++ Scenes/obsticle.gd
@@ -1,13 +1,12 @@
 extends CharacterBody3D
 
-
 @export var max_speed : float = 60
 @export var accelaration : float = 1
 
 func _physics_process(delta):
-	velocity.z = lerpf(velocity.z, 60*randf_range(1,2), accelaration*delta)
-	rotation_degrees.x = velocity.z
-	rotation_degrees.z = velocity.z/2
+	velocity.z = lerpf(velocity.z, 60 * randf_range(1,2), accelaration*delta)
+	rotation_degrees.x += 0.75
+	rotation_degrees.z += 0.5
 	move_and_slide()
 	if transform.origin.z >= 20:
 		queue_free()
--- Scenes/obsticle_spawner.gd
+++ Scenes/obsticle_spawner.gd
@@ -2,12 +2,13 @@ extends Node3D
 
 @export var offsetX : float = 6
 @export var offsetY : float = 5
-
+@export var amount : int = 3
 @export var Obsticles : Array[PackedScene]
 
 func _on_spawn_timer_timeout():
-	var current_obsticle = Obsticles.pick_random().instantiate()
-	current_obsticle.transform.origin = transform.origin
-	current_obsticle.transform.origin.x += randf_range(offsetX,-offsetX)
-	current_obsticle.transform.origin.y += randf_range(offsetY,-offsetY)
-	get_tree().current_scene.add_child(current_obsticle)
+	for i in amount:
+		var current_obsticle = Obsticles.pick_random().instantiate()
+		current_obsticle.transform.origin = transform.origin
+		current_obsticle.transform.origin.x += randf_range(offsetX,-offsetX)
+		current_obsticle.transform.origin.y += randf_range(offsetY,-offsetY)
+		get_tree().current_scene.add_child(current_obsticle)
